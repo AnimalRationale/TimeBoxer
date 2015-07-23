8954eae25941eec6fa2cf26a8c0a0bd3002f96f8
AnimalRationale <wojciech.mardyla@gmail.com>
2015-07-23 18:37:06 +0200

    Init for acquiring and releasing wake lock (WakefulReceiver class).

---
+++ app/src/main/java/pl/appnode/timeboxer/WakefulReceiver.java
@@ -0,0 +1,50 @@
+package pl.appnode.timeboxer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.util.Log;
+
+
+public class WakefulReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "WakefulReceiver";
+    private static final String LOCK_TAG = "pl.appnode.timeboxer";
+    private static PowerManager.WakeLock sWakeLock = null;
+
+    private static synchronized void acquireWakeLock (Context context) {
+        if (sWakeLock == null) {
+            Log.d(TAG, "Wake lock == null, setting up.");
+            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            sWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, LOCK_TAG);
+            sWakeLock.setReferenceCounted(true);
+        }
+        sWakeLock.acquire();
+        Log.d(TAG, "Wake lock acquired.");
+    }
+
+    public static synchronized void releaseLock() {
+        if (sWakeLock != null) {
+            Log.d(TAG, "Releasing wakelock.");
+            try {
+                sWakeLock.release();
+                Log.d(TAG, "Wake lock released.");
+            } catch (Throwable thex) {
+                Log.d(TAG, "Wake lock catch.");
+                // wakeLock should be already released
+            }
+        } else {
+            Log.d(TAG, "Wakelock null.");
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent alarmIntent) {
+        acquireWakeLock(context);
+        Intent serviceIntent = new Intent(context, TimersBroadcastService.class);
+        Log.d(TAG, "Starting service.");
+        context.startService(serviceIntent);
+        Log.d(TAG, "Service started.");
+    }
+}

71797957a64f87413611487b7217298bd2d9f0d3
AnimalRationale <wojciech.mardyla@gmail.com>
2015-09-10 20:07:06 +0200

    ActionBar with app icon in AppIntro.

--- app/src/main/java/pl/appnode/timeboxer/AppIntroActivity.java
+++ app/src/main/java/pl/appnode/timeboxer/AppIntroActivity.java
@@ -1,8 +1,11 @@
 package pl.appnode.timeboxer;
 
+import android.support.v7.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
@@ -11,6 +14,10 @@ public class AppIntroActivity extends AppIntro2 {
 
     @Override
     public void init(Bundle savedInstanceState) {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
 
         // Slides: title, description, image, background colour
         addSlide(AppIntroFragment.newInstance(getString(R.string.introduction_slide01_title),
@@ -27,7 +34,17 @@ public class AppIntroActivity extends AppIntro2 {
 
     @Override
     public void onDonePressed() {
-        Intent intent = new Intent(AppContextHelper.getContext(), MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

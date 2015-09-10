57ae55a38e33ecfa93ccf3f1aee76e09e8399398
AnimalRationale <wojciech.mardyla@gmail.com>
2015-09-10 19:34:56 +0200

    Slides strings in resources, cleanup.

--- app/src/main/java/pl/appnode/timeboxer/AppIntroActivity.java
+++ app/src/main/java/pl/appnode/timeboxer/AppIntroActivity.java
@@ -12,11 +12,16 @@ public class AppIntroActivity extends AppIntro2 {
     @Override
     public void init(Bundle savedInstanceState) {
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest
-        addSlide(AppIntroFragment.newInstance("First slide", "Set timer", R.mipmap.ic_launcher, Color.parseColor("#8BC34A")));
-        addSlide(AppIntroFragment.newInstance("Second slide", "Set ringtone and volume", R.mipmap.ic_launcher, Color.parseColor("#009688")));
-        addSlide(AppIntroFragment.newInstance("Third slide", "Start/stop timer", R.mipmap.ic_launcher, Color.parseColor("#FFC107")));
+        // Slides: title, description, image, background colour
+        addSlide(AppIntroFragment.newInstance(getString(R.string.introduction_slide01_title),
+                getString(R.string.introduction_slide01_description),
+                R.mipmap.ic_launcher, Color.parseColor("#8BC34A")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.introduction_slide02_title),
+                getString(R.string.introduction_slide02_description),
+                R.mipmap.ic_launcher, Color.parseColor("#009688")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.introduction_slide03_title),
+                getString(R.string.introduction_slide03_description),
+                R.mipmap.ic_launcher, Color.parseColor("#FFC107")));
         showDoneButton(true);
     }
 

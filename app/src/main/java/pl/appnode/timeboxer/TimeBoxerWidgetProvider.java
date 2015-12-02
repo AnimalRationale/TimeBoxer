c70ac0bf56d5728962810739241ba1757b24fa4b
AnimalRationale <wojciech.mardyla@gmail.com>
2015-12-02 21:35:29 +0000

    Cleanup in WidgetProvider (refactored loop for checking button clicks, comments).

--- app/src/main/java/pl/appnode/timeboxer/TimeBoxerWidgetProvider.java
+++ app/src/main/java/pl/appnode/timeboxer/TimeBoxerWidgetProvider.java
@@ -8,9 +8,12 @@ import android.util.Log;
 
 import static pl.appnode.timeboxer.Constants.WIDGET_BUTTON_ACTION;
 
+/**
+ * Handles user actions in TimeBoxer's widget .
+ */
 public class TimeBoxerWidgetProvider extends android.appwidget.AppWidgetProvider {
 
-    private static final String TAG = "WidgetProvider";
+    private static final String LOGTAG = "WidgetProvider";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -20,22 +23,20 @@ public class TimeBoxerWidgetProvider extends android.appwidget.AppWidgetProvider
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        Log.d(TAG, "Widget onReceive.");
-        int i = 1;
-        while (i != 5) {
+        // Checks clicked button, assigns timer id and calls/starts TimersService for proper action
+        for (int i = 1; i < 5; i++) {
             if (WIDGET_BUTTON_ACTION[i].equals(intent.getAction())) {
                 i = 5;
                 int j = Integer.parseInt(intent.getAction());
                 if (TimersService.sTimersList != null) {
                     TimersService.timerAction(Integer.parseInt(intent.getAction()));
-                    Log.d(TAG, "Timers list not null. Action: " + j);}
+                }
                 else {
-                    Log.d(TAG, "Timers list null. Action: " + j);
                     Intent commandIntent = new Intent(context, TimersService.class);
                     commandIntent.putExtra("buttonId", j);
                     context.startActivity(commandIntent);
                 }
-            } else i++;
+            }
         }
     }
 }
